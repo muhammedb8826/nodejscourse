8ca890f20244229bcf3367ddb9ed92a78b0572ee
Muhammed Berisso <muhammedb8826@gmail.com>
2023-01-01 23:53:29 +0300

    my first rest api completed

--- app.js
+++ app.js
@@ -1,11 +1,63 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-  if (req.url == '/') {
-    res.end('Welcome to our home page');
-  }
-  if (req.url == '/about') {
-    res.end('About page');
-  }
+const Joi = require('joi');
+const express = require('express');
+const app = express();
+app.use(express.json());
+const courses = [
+  { id: 1, name: 'course1' },
+  { id: 2, name: 'course2' },
+  { id: 3, name: 'course3' },
+];
+app.get('/', (req, res) => {
+  res.send('Hello welcome');
 });
-server.listen(5000);
+app.get('/api/courses/', (req, res) => {
+  res.send(courses);
+});
+
+app.get('/api/courses/:id', (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    res.status(404).send('The course you are looking for was not fond.');
+  res.send(course);
+});
+
+app.post('/api/courses', (req, res) => {
+  const { error } = validateCourse(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const course = { id: courses.length + 1, name: req.body.name };
+  courses.push(course);
+  res.send(course);
+});
+
+app.put('/api/courses/:id', (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    return res.status(404).send('The course with the given id was not found');
+
+  const { error } = validateCourse(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  course.name = req.body.name;
+  res.send(course);
+});
+
+function validateCourse(course) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+  return schema.validate(course);
+}
+
+app.delete('/api/courses/:id', (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    return res.status(404).send('The course with the given id was not found.');
+
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+  res.send(course);
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
